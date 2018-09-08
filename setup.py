c29717c73a8e141b299456264722683b595b3643
askainet <ivan@askai.net>
2018-09-08 16:06:10 +0200

    Add tests

--- setup.py
+++ setup.py
@@ -8,10 +8,6 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aws-stash",
-    setup_requires=[
-        'setuptools_scm',
-        'moto'
-    ],
     use_scm_version={
         'version_scheme': 'guess-next-dev',
         'local_scheme': 'dirty-tag'
